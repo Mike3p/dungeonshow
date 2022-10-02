4b46bcfa50757655d5eaf4b6d57a229e1d9dfc80
Ayonix <github@leva.xyz>
2022-10-02 23:43:04 +0200

    Introduce code splitting for generator yamls to reduce load times

--- src/routes/character_generator/+page.ts
+++ src/routes/character_generator/+page.ts
@@ -1,16 +1,29 @@
-import basicGeneratorDict from '$lib/data/generator_basic_acks.yaml';
-import circleOfDawnDict from '$lib/data/generator_circle_of_dawn.yaml';
 import type { GeneratorDict } from '$lib/generator_dict/generator_dict';
 
 type DataType = {
-	generatorDicts: { [name: string]: GeneratorDict };
+	generatorDicts: Mapping;
 };
 
+type Mapping = { [name: string]: GeneratorDict };
+
+async function importDicts() {
+	return (
+		await Promise.all([
+			{ name: 'Base', dict: (await import('$lib/data/generator_basic_acks.yaml')).default },
+			{
+				name: 'Circle of Dawn',
+				dict: (await import('$lib/data/generator_circle_of_dawn.yaml')).default
+			}
+		])
+	).reduce<Mapping>((acc, dict) => {
+		acc[dict.name] = dict.dict as GeneratorDict;
+		return acc;
+	}, {});
+}
+
 export async function load(): Promise<DataType> {
+	const generatorDicts = await importDicts();
 	return {
-		generatorDicts: {
-			Basic: basicGeneratorDict,
-			'Circle of Dawn': circleOfDawnDict
-		}
+		generatorDicts: generatorDicts
 	};
 }
