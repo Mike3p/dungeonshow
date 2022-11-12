285990fc1cb476c94d298aad0f5d247994728bfe
Ayonix <github@leva.xyz>
2022-11-12 15:29:26 +0100

    Wrap menu button in a button to have better accessibility (maybe)

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -6,21 +6,13 @@
 <AppBar>
 	<svelte:fragment slot="lead">
 		<div class="lg:hidden cursor-pointer">
-			<svg
-				on:click={() => menuStore.set(true)}
-				xmlns="http://www.w3.org/2000/svg"
-				x="0px"
-				y="0px"
-				viewBox="0 0 576 512"
-				class="svg-icon inline-block outline-none fill-black dark:fill-white w-6 h-6"
-				focusable="false"
-				tabindex="-1"
-				data-testid="svg-icon"
-			>
-				<path
-					d="M0 96C0 78.33 14.33 64 32 64H416C433.7 64 448 78.33 448 96C448 113.7 433.7 128 416 128H32C14.33 128 0 113.7 0 96zM0 256C0 238.3 14.33 224 32 224H416C433.7 224 448 238.3 448 256C448 273.7 433.7 288 416 288H32C14.33 288 0 273.7 0 256zM416 448H32C14.33 448 0 433.7 0 416C0 398.3 14.33 384 32 384H416C433.7 384 448 398.3 448 416C448 433.7 433.7 448 416 448z"
-				/>
-			</svg>
+			<button on:click={() => menuStore.set(true)} tabindex="0">
+				<svg viewBox="0 0 100 80" class="w-6 h-6 m-1">
+					<rect width="100" height="20" rx="10" />
+					<rect y="30" width="100" height="20" rx="10" />
+					<rect y="60" width="100" height="20" rx="10" />
+				</svg>
+			</button>
 		</div>
 		<div class="flex flex-col justify-evenly">
 			<h1>The Dungeon Show</h1>
