c2432ef82acc5364ade6837db158e2eab7336300
Ayonix <github@leva.xyz>
2023-08-22 23:00:25 +0200

    Limit max width to 7xl

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,13 +6,15 @@
 	import Nav from './Nav.svelte';
 </script>
 
-<div class="flex flex-col h-screen w-screen overflow-auto">
-	<Header />
-	<div class="flex flex-1">
-		<main class="p-4 flex-1">
-			<slot />
-		</main>
+<div class="flex flex-col items-center h-screen w-screen overflow-auto bg-slate-300">
+	<div class="flex flex-col w-full flex-1 max-w-7xl bg-white">
+		<Header />
+		<div class="flex flex-1">
+			<main class="p-4 flex-1">
+				<slot />
+			</main>
+		</div>
+		<Footer />
+		<Nav position="bottom" />
 	</div>
-	<Footer />
-	<Nav position="bottom" />
 </div>
