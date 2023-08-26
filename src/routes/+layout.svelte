562fe8242894c51b90192cc3202b096d9b40b25a
Ayonix <github@leva.xyz>
2023-08-26 22:33:54 +0200

    Scrollbar is for the whole page again

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,14 +7,16 @@
 </script>
 
 <div class="bg-slate-300 h-full pb-16 lg:pb-0">
-	<div class="flex flex-col w-full h-full flex-1 max-w-7xl mx-auto overflow-auto bg-white">
-		<Header />
-		<div class="flex flex-1">
-			<main class="p-4 flex-1">
-				<slot />
-			</main>
+	<div class="overflow-auto h-full">
+		<div class="flex flex-col w-full h-full max-w-7xl mx-auto">
+			<Header />
+			<div class="flex flex-1">
+				<main class="p-4 flex-1 bg-white">
+					<slot />
+				</main>
+			</div>
+			<Footer />
+			<Nav position="bottom" />
 		</div>
-		<Footer />
-		<Nav position="bottom" />
 	</div>
 </div>
