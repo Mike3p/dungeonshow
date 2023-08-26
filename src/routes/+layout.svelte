0fc0733da20ac67bf8de781eb952bdcb1739183d
Ayonix <github@leva.xyz>
2023-08-26 23:09:48 +0200

    Apply scrollbar-gutter only when the page is wide enough

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,8 @@
 </script>
 
 <div class="bg-slate-300 h-full pb-16 lg:pb-0">
-	<div class="overflow-auto h-full stable-scroll">
-		<div class="flex flex-col w-full h-full max-w-7xl mx-auto">
+	<div class="overflow-auto h-full xl:stable-scroll">
+		<div class="flex flex-col w-full h-full max-w-5xl mx-auto">
 			<Header />
 			<div class="flex flex-1">
 				<main class="p-4 flex-1 bg-white">
@@ -20,9 +20,3 @@
 		</div>
 	</div>
 </div>
-
-<style>
-	.stable-scroll {
-		scrollbar-gutter: stable;
-	}
-</style>
