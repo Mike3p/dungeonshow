36817d45882577d878018e2fa7bc6c58c3c20e2f
Ayonix <github@leva.xyz>
2023-08-26 23:41:18 +0200

    Add some disabled button styles

--- src/lib/components/forms/Button.svelte
+++ src/lib/components/forms/Button.svelte
@@ -3,13 +3,13 @@
 	export let color: 'primary' | 'red' = 'primary';
 
 	const filledPrimary =
-		'text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800';
+		'text-white bg-primary-700 enabled:hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:enabled:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800';
 	const outlinePrimary =
-		'text-primary-700 hover:text-white border border-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-primary-500 dark:text-primary-500 dark:hover:text-white dark:hover:bg-primary-500 dark:focus:ring-primary-800';
+		'text-primary-700 enabled:hover:text-white border border-primary-700 enabled:hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-primary-500 dark:text-primary-500 dark:enabled:hover:text-white dark:enabled:hover:bg-primary-500 dark:focus:ring-primary-800';
 	const filledRed =
-		'text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800';
+		'text-white bg-red-700 enabled:hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-red-600 dark:enabled:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800';
 	const outlineRed =
-		'text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-500 dark:focus:ring-red-800';
+		'text-red-700 enabled:hover:text-white border border-red-700 enabled:hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:enabled:hover:text-white dark:enabled:hover:bg-red-500 dark:focus:ring-red-800';
 
 	function getClasses(outline: boolean, color: 'primary' | 'red') {
 		if (outline) {
@@ -31,4 +31,8 @@
 	const variantClasses = getClasses(outline, color);
 </script>
 
-<button {...$$restProps} on:click class={variantClasses}><slot /></button>
+<button
+	{...$$restProps}
+	on:click
+	class={`${variantClasses} disabled:opacity-60 disabled:cursor-not-allowed`}><slot /></button
+>
