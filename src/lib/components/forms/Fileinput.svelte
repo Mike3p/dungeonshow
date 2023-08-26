c1dee85e1191ad828b015fbb7696720af440daa7
Ayonix <github@leva.xyz>
2023-08-26 23:57:07 +0200

    Style fileinput as primary button

--- src/lib/components/forms/Fileinput.svelte
+++ src/lib/components/forms/Fileinput.svelte
@@ -1,1 +1,10 @@
-<input type="file" on:change {...$$restProps} />
+<script lang="ts">
+	export let label: string;
+</script>
+
+<label
+	class="disabled:opacity-60 disabled:cursor-not-allowed text-white bg-primary-700 enabled:hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:enabled:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+>
+	<input type="file" on:change {...$$restProps} class="hidden" />
+	{label}
+</label>
