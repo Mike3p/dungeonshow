cd13e4ddcf47ab1141da745d2f052c7902290c73
Ayonix <github@leva.xyz>
2023-08-26 23:00:54 +0200

    Add hover to menu; consolidate code

--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -13,30 +13,17 @@
 	$: activeUrl = $page.url.pathname;
 </script>
 
-{#if position === 'bottom'}
-	<div class="fixed h-16 lg:hidden bg-primary-400 bottom-0 w-full">
-		<div class="grid max-w-lg mx-auto grid-cols-3 h-full">
-			{#each navOptions as { href, name, icon }}
-				<a
-					{href}
-					class={`flex flex-col p-2 items-center text-white ${
-						href === activeUrl ? 'bg-primary-500' : ''
-					}`}
-					data-sveltekit-preload-data
-				>
-					<Icon name={icon} />
-					<span>{name}</span>
-				</a>
-			{/each}
-		</div>
-	</div>
-{:else}
-	<div class="hidden lg:grid grid-cols-3 justify-center bg-primary-400">
+<div
+	class="{position === 'bottom'
+		? 'fixed lg:hidden bottom-0 w-full h-16'
+		: 'hidden lg:grid h-full'}  bg-primary-400"
+>
+	<div class="grid max-w-lg mx-auto grid-cols-3 h-full font-medium">
 		{#each navOptions as { href, name, icon }}
 			<a
 				{href}
-				class={`flex flex-col p-2 items-center justify-center text-white ${
-					href === activeUrl ? 'bg-primary-500' : ''
+				class={`flex flex-col p-2 items-center justify-center text-white hover:bg-primary-300 active:bg-primary-600 ${
+					href === activeUrl ? 'bg-primary-500 hover:bg-primary-600' : ''
 				}`}
 				data-sveltekit-preload-data
 			>
@@ -45,4 +32,4 @@
 			</a>
 		{/each}
 	</div>
-{/if}
+</div>
