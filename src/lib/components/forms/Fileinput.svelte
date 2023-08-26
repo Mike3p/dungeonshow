df6622c7bf93b253932824c2397099c5f8f8ff1a
Ayonix <github@leva.xyz>
2023-08-27 00:24:20 +0200

    Fix hover and disabled for fileInput

--- src/lib/components/forms/Fileinput.svelte
+++ src/lib/components/forms/Fileinput.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <label
-	class="cursor-pointer disabled:opacity-60 disabled:cursor-not-allowed text-white bg-primary-700 enabled:hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:enabled:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+	class="cursor-pointer text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
 >
 	<input type="file" on:change {...$$restProps} class="hidden" />
 	{label}
