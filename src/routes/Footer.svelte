844c2216d4f7807757de7e9a69d1f2629a63b36c
Ayonix <github@leva.xyz>
2024-03-29 21:27:24 +0100

    Update dependencies

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,3 +1,3 @@
 <footer class="h-10 bg-slate-200 dark:bg-slate-700 flex flex-shrink-0 justify-center items-center">
-	<p class="text-sm italic text-neutral-400">2022</p>
+	<p class="text-sm italic text-neutral-400">2024</p>
 </footer>
